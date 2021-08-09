adcf98361d5e62de7428c2120dfdfa9baa7accaa
dpbam <derekphtpt@gmail.com>
2021-08-09 14:36:41 -0600

    files partially set up

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
 const path = require('path');
+const express = require('express');
+const routes = require('/routes');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+require('dotenv').config
+// const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// turn on routes
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static('views'));
+
+app.use(routes);
+
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log("Now listening"));
+  });
